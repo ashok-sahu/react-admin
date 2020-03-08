79b2ce51189eed459a0c73953ef280f6f4c8bd6d
ashok-sahu <ashoksahu1105@gmail.com>
2020-03-09 00:30:32 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Header from './Header'
+import Menu from './Menu'
+import Footer from './Footer'
+import Content from './Content';
+
+function App() {
+  return (
+    <>
+    <Header/>
+    <Menu/>
+    <Content/>
+    <Footer/>
+    </>
+  )
+}
+
+export default App;
